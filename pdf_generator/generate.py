3958db8fef878eeb50772d7284b5a1a77de9b353
jawahar <jawaharsivakumar02@gmail.com>
2025-07-16 06:11:08 +0530

    pack

--- pdf_generator/generate.py
+++ pdf_generator/generate.py
@@ -2,7 +2,7 @@ from json_file_gen import make_file
 from pre_processing import categorize_products_with_cohere, refine_product_json
 
 
-def generate_json(results):
+def generate_json(results,output_file="output.json"):
     products = ""
     filetered_list = {}
     for i in range(0,len(results)):
@@ -12,4 +12,4 @@ def generate_json(results):
     json_output = []
     for k in list(catogories):
         json_output.append({"category name": k ,"products":[refine_product_json({"product name":i,"description":filetered_list[i]}) for i in catogories[k]]})
-    make_file(json_output)
+    make_file(json_output,output_file)
